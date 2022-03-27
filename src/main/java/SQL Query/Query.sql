335d155337f0948a688c71e5dcf675c2e838ad7f
Diego <diegoac053@gmail.com>
2022-03-26 21:18:10 -0600

    Query to insert data, DB is created with application

---
+++ src/main/java/SQL Query/Query.sql
@@ -0,0 +1,23 @@
+USE db_employees;
+
+INSERT INTO employees(date, email, gender, last_name, name, phone, salary) VALUES
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230),
+("2022-05-03", "juanperez@gmail.com", "Male", "Perez", "Juan", "79797979", 230);
+
+SELECT * FROM employees
