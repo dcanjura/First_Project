e616f979d5aa51700346483065374845c8e2db1e
Diego <diegoac053@gmail.com>
2022-03-26 21:12:46 -0600

    Added export to pdf and excel files

--- src/main/java/demo/controller/employeeController.java
+++ src/main/java/demo/controller/employeeController.java
@@ -2,6 +2,8 @@ package demo.controller;
 
 import demo.Service.employeeService;
 import demo.entity.Employees;
+import demo.reports.employeeExporterExcel;
+import demo.reports.employeeExporterPDF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,7 +19,13 @@ import demo.pagination.pageRender;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,7 +43,7 @@ public class employeeController {
             return "redirect:/listing";
         }
         model.put("employee", employee);
-        model.put("title", "Employee Details" + employee.getName());
+        model.put("title", "Employee Details " + employee.getName() + " " + employee.getLastname());
 
         return "view";
     }
@@ -106,5 +114,31 @@ public class employeeController {
         }
 
         return "redirect:/listing";
+    }
+
+    @GetMapping("/exportPDF")
+    public void exportEmployeeListPDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDate = format.format(new Date());
+        String header = "Content-Disposition";
+        String value = "attachment; filename=Employees_" + currentDate + ".pdf";
+        response.setHeader(header, value);
+        List<Employees> employees = employeeService.findAll();
+        employeeExporterPDF exporterPDF = new employeeExporterPDF(employees);
+        exporterPDF.exportFile(response);
+    }
+
+    @GetMapping("/exportExcel")
+    public void exportEmployeeListExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDate = format.format(new Date());
+        String header = "Content-Disposition";
+        String value = "attachment; filename=Employees_" + currentDate + ".xlsx";
+        response.setHeader(header, value);
+        List<Employees> employees = employeeService.findAll();
+        employeeExporterExcel exporterExcel = new employeeExporterExcel(employees);
+        exporterExcel.export(response);
     }
 }
